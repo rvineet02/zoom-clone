d2caba9312ff1e40d50763534e9b9064b5714ba0
vineet <rvineet02@gmail.com>
2021-01-30 09:41:21 -0500

    self video working

--- server.js
+++ server.js
@@ -4,7 +4,10 @@ const server = require('http').Server(app);
 const { v4: uuidV4 } = require('uuid');
 
 
+
 app.set('view engine', 'ejs');
+app.use(express.static('public'));
+
 
 app.get('/', (req, res) => {
     res.redirect(`/${uuidV4()}`)
